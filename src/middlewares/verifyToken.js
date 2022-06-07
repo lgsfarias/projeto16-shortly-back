ea619a3eacec5a480458ad5af09d9df23765eb53
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-06-07 02:15:04 -0300

    fix: verify if authorization header does not exist

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -7,28 +7,28 @@ import UsersRepository from '../repositories/UsersRepository.js';
 const verifyToken = async (req, res, next) => {
     const { authorization } = req.headers;
 
-    if (authorization.slice(0, 7) !== 'Bearer ') {
-        return res.status(422).json({
-            message: 'Invalid token',
-        });
-    }
-
     if (!authorization) {
         return res.status(401).json({
             message: 'Missing token',
         });
     }
 
+    if (authorization.slice(0, 7) !== 'Bearer ') {
+        return res.status(422).json({
+            message: 'Invalid token',
+        });
+    }
+
     const token = authorization.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        const user = await UsersRepository.getUserById(decoded.id);
+        const user = await UsersRepository.getUserById(decoded.userId);
 
         if (!user) {
             return res.status(401).json({
-                message: 'Invalid token',
+                message: 'Invalid token user',
             });
         }
 
@@ -36,9 +36,7 @@ const verifyToken = async (req, res, next) => {
 
         next();
     } catch (error) {
-        return res.status(401).json({
-            message: 'Invalid token',
-        });
+        res.status(500).json(error);
     }
 };
 
